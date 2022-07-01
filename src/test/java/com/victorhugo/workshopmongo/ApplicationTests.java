9a373855e6e571ec0d25cd2cd0f2ba95734e34e7
Vhmarega <victorhugomarega@hotmail.com>
2022-07-01 15:35:15 -0300

    Entity User e REST funcionando

--- src/test/java/com/victorhugo/workshopmongo/ApplicationTests.java
+++ src/test/java/com/victorhugo/workshopmongo/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.victorhugo.workshop.mongo.spring;
+package com.victorhugo.workshopmongo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
